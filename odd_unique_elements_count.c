29fc55d38448df156414e48e4dd961e4298efdc0
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-06-04 11:30:33 +0530

    #include<stdio.h> int main() {     int n,a[100],i,j,c=0,m=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=i;j<n;j++)         {             if(a[i]==a[j])             {                 c=c+1;             }         }         if(c==1)         {             if(a[i]%2!=0)             {                 m=m+1;             }         }     }     printf("%d",m);     return 0; }

---
+++ odd_unique_elements_count.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,j,c=0,m=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c=c+1;
+            }
+        }
+        if(c==1)
+        {
+            if(a[i]%2!=0)
+            {
+                m=m+1;
+            }
+        }
+    }
+    printf("%d",m);
+    return 0;
+}
