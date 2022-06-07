c748a19ff76290dd935f32ff49c1834db185a53e
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-06-07 21:29:57 +0530

    #include<stdio.h> int main() {     int n,a[100],i,r,k,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         k=a[i];         while(k>0)         {             r=k%10;             sum=sum+r;             k=k/10;         }     }     printf("%d",sum);     return 0; }

---
+++ Play_with_digits.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,r,k,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        k=a[i];
+        while(k>0)
+        {
+            r=k%10;
+            sum=sum+r;
+            k=k/10;
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
