6c68dcc2830927dd3839ac858068b738aeab3610
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-06-30 12:18:17 +0530

    #include<stdio.h> int main() {     int a=0,b=1,c,i,n;     scanf("%d",&n);     printf("%d %d ",a,b);     c=a+b;     for(i=1;i<(n-1);i++)     {         printf("%d ",c);         a=b;         b=c;         c=a+b;     }     return 0; }

--- fibonacci.c
+++ fibonacci.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a=0,b=1,c,n,i;
+    int a=0,b=1,c,i,n;
     scanf("%d",&n);
     printf("%d %d ",a,b);
+    c=a+b;
     for(i=1;i<(n-1);i++)
     {
-        c=a+b;
         printf("%d ",c);
         a=b;
         b=c;
+        c=a+b;
     }
     return 0;
-}   
+}
