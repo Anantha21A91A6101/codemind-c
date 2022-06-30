c48cf492482474f81bfa600006dbeb83cc0f1bcc
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-06-30 15:42:23 +0530

    #include<stdio.h> int main() {     int n,a[100],i,sum=0,c=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum=sum+a[i];     }     avg=float(sum)/float(n);     for(i=0;i<n;i++)     {         if(a[i]<=avg)         {             c++;         }     }     printf("%d",c);     return 0; }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,sum=0,c=0;
+    float avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    avg=float(sum)/float(n);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
