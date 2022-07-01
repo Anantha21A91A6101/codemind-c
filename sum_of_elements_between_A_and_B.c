8bbedf7403eff4cdf40832e276dbbc0b1102b3d0
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-07-01 11:46:03 +0530

    #include<stdio.h> int main() {     int n,a[100],i,k,l,sum=0,add=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d %d",&k,&l);     for(i=0;i<n;i++)     {         if(a[i]>=k&&a[i]<=l)         {             sum=sum+a[i];         }         else         {             add=add+a[i];         }     }     printf("%d",sum);     return 0; }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,k,l,sum=0,add=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d %d",&k,&l);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=k&&a[i]<=l)
+        {
+            sum=sum+a[i];
+        }
+        else
+        {
+            add=add+a[i];
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
