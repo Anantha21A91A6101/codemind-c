cf73f7eb81061be620c427c102475fef8e354bb6
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-07-15 09:56:15 +0530

    #include<stdio.h> int main() {     int n,a[100],i,r,c=0,k,s=0,m;     scanf("%d %d",&n,&m);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         k=a[i];         if(k==0)         {             c=c+1;         }         else         {             while(k!=0)             {                 r=k%10;                 c++;                 k=k/10;             }         }         if(c==m)         {             s++;         }     }     printf("%d",s);     return 0; }

--- K_digit_length_numbers_in_a_array.c
+++ K_digit_length_numbers_in_a_array.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,c=0,k,r,m,sum=0;
+    int n,a[100],i,r,c=0,k,s=0,m;
     scanf("%d %d",&n,&m);
     for(i=0;i<n;i++)
-      {
-         scanf("%d",&a[i]);
-      }
-      for(i=0;i<n;i++)
-      {
-         c=0;
-         k=a[i];
-         if(k==0)
-           {
-              c=c+1;
-           }
-         else
-          {
-              while(k!=0)
-              {
-                 r=k%10;
-                 c=c+1;
-                 k=k/10;
-              }
-          }
-          if(m==c)
-          {
-              sum++;
-          }
-       }
-       printf("%d",sum);
-       return 0;
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        k=a[i];
+        if(k==0)
+        {
+            c=c+1;
+        }
+        else
+        {
+            while(k!=0)
+            {
+                r=k%10;
+                c++;
+                k=k/10;
+            }
+        }
+        if(c==m)
+        {
+            s++;
+        }
+    }
+    printf("%d",s);
+    return 0;
 }
