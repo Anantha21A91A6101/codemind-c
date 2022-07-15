c6c0c5013584c7a3f7ba948bb31c47e1a30d1a39
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-07-15 09:54:12 +0530

    #include<stdio.h> int main() {     int n,a[100],i,r,c=0,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         k=a[i];         if(k==0)         {             c=c+1;         }         else         {             while(k!=0)             {                 r=k%10;                 c++;                 k=k/10;             }         }         printf("%d ",c);     }     return 0; }

--- count_digits.c
+++ count_digits.c
@@ -1,29 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,c=0,k,r;
+    int n,a[100],i,r,c=0,k;
     scanf("%d",&n);
     for(i=0;i<n;i++)
-      {
-         scanf("%d",&a[i]);
-      }
-      for(i=0;i<n;i++)
-      {
-         c=0;
-         k=a[i];
-         if(k==0)
-           {
-              c=c+1;
-           }
-         else
-          {
-              while(k!=0)
-              {
-                 r=k%10;
-                 c=c+1;
-                 k=k/10;
-              }
-          }
-          printf("%d ",c);
-       }
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        k=a[i];
+        if(k==0)
+        {
+            c=c+1;
+        }
+        else
+        {
+            while(k!=0)
+            {
+                r=k%10;
+                c++;
+                k=k/10;
+            }
+        }
+        printf("%d ",c);
+    }
+    return 0;
 }
