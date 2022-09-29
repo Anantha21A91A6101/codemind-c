a12e56edd16f49a0541a61a5c50856ee9c42dfe3
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-09-29 21:38:10 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     int k=n-1;     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(i==j || j==0 || i+j==k)             {                 printf("* ");             }             else             {                 printf("  ");             }         }         printf(" ");         k++;     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,21 +3,22 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
+    int k=n-1;
     for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++)
         {
-            if(i==j)
+            if(i==j || j==0 || i+j==k)
             {
-                printf("0");
+                printf("* ");
             }
             else
             {
-                printf("x");
+                printf("  ");
             }
         }
         printf("
 ");
+        k++;
     }
-    return 0;
 }
